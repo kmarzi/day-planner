7bbfbfd8b4313a2e3a66dcd0a0a1b134ee4b1f47
Kmarzi <k.l.marziali@gmail.com>
2020-08-11 22:30:10 -0400

    corrected variables and added local storage function

--- assets/script.js
+++ assets/script.js
@@ -1,56 +1,51 @@
 //make variables:
 var hour = moment().format("H")
-var getSetColors = "";
+var button = $(".saveBtn") 
 
 var moment = moment();
 var currentDay = moment.format('dddd, MMMM Do, YYYY');
 $("#currentDay").text(currentDay);
 
 //make on clicks
-$("button").on("click", function () {
+
+button.on("click", function () {
+    console.log("hit")
     var userInput = $(this).siblings(".description").val();
-    if (userInput !== "") {
         var mainRow = $(this).parent().attr("id");
-        savedTask(userInput, mainRow);
-    }
+        localStorage.setItem(mainRow, userInput)
 });
-function savedTask() {
-    $("div").each(function (index, value) {
-        var divId = this.id;
-        if (divId.includes("time")) {
-            var savedInfo = window.localStorage.getItem(divId)
-            if (savedInfo != null) {
-                $(this).children(".description").val(savedInfo)
-            }
-        }
-        });
 
-    function getSetColors() {
-        $("div").each(function (index, value) {
-            var divId = this.id;
-            if (divId.includes("time")) {
-                var divTime = parseInt(divId);
-            }
-        });
-        getSetColors();
+$("#9 .description").val(localStorage.getItem("9"))
+$("#10 .description").val(localStorage.getItem("10"))
+$("#11 .description").val(localStorage.getItem("11"))
+$("#12 .description").val(localStorage.getItem("12"))
+$("#13 .description").val(localStorage.getItem("13"))
+$("#14 .description").val(localStorage.getItem("14"))
+$("#15 .description").val(localStorage.getItem("15"))
+$("#16 .description").val(localStorage.getItem("16"))
+$("#17 .description").val(localStorage.getItem("17"))
 
-        if(divTime < h) {
+
+
+function getSetColors() {
+    $(".time-block").each(function () {
+        var divId = $(this).attr("id")
+        var divTime = parseInt(divId);
+        if(divTime < hour) {
             $(this).children(".description").attr("class", "col-md-10 description past");
-        } else if (divTime == h) {
+        } else if (divTime == hour) {
             $(this).children(".description").attr("class", "col-md-10 description present");
         } else {
             $(this).children(".description").attr("class", "col-md-10 description future");
         };
+    });
 
-        function setTimeInterval() {
-            window.setInterval(getSetColors, 3000);
-        };
-        setTimeInterval();
-
-        function savedTask(userInput, mainRow) {
-            window.localStorage.removeItem(mainRow);
-            window.localStorage.setItem(mainRow, userInput)
-        };
-
-    }};
+};
     
+getSetColors()
+
+function setTimeInterval() {
+    window.setInterval(getSetColors, 3000);
+};
+
+setTimeInterval();
